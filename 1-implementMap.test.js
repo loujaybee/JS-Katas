c6a8861db03d36eddee7be76e61a02e79953c276
louisbichard <contact@louisjohnbichard.co.uk>
2018-06-09 09:51:31 +0100

    added pick

--- 1-implementMap.test.js
+++ 1-implementMap.test.js
@@ -6,5 +6,18 @@ describe('Implement .Map', () => {
         const result = map([{ age: 12}], item => ({ ...item, age: item.age + 1}));
         expect(result).toEqual([ { age: 13 }]);
     });
-    
+
+    test('Do not mutate original array', () => {
+        const original = [{ age: 12}];
+        const result = map(original, item => ({ ...item, age: item.age + 1}));
+        
+        expect(original).toEqual([ { age: 12 }]);
+        expect(result).toEqual([ { age: 13 }]); 
+    });
+
+    test('Pass string as second argument', () => {
+        const result = map([{ age: 12 }], 'age');
+        expect(result).toEqual([ 12 ]);
+    });
+
 });
