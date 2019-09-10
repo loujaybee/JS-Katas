f0f4d2660a9b12dfcc03946ff83c847fbd8c8f6d
Lou Bichard <contact@louisjohnbichard.co.uk>
2019-09-10 13:41:02 +0100

    Update 1-implementMap-question.js

--- 1-implementMap-question.js
+++ 1-implementMap-question.js
@@ -2,11 +2,14 @@
 /* 
 
     Implement a map function for an array, similar to the .map property found in core JS.
+    
+    You can implement as a basic method, or extend the prototype if needed. 
 
     Parameter 1) Data is passed as an array
-    Parameter 2) A callback that implements the 
+    Parameter 2) A callback that implements the behaviour
 
     Note: Consider immutability
+    
 */ 
 var map = function(data, func){
     // Your code here
